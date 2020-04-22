1d028a79a4e57cbb96b695930ef4e7af7d2f0375
matheusfrancisco <matheusmachadoufsc@gmail.com>
2020-04-21 23:00:50 -0300

    Bind jgit java from clojure project

--- project.clj
+++ project.clj
@@ -6,10 +6,16 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/data.json "0.2.7"]
                  [clj-jgit "1.0.0-beta3"]
+                 [org.eclipse.jgit/org.eclipse.jgit "5.5.1.201910021850-r"]
+                 [org.eclipse.jgit/org.eclipse.jgit.ssh.apache "5.5.1.201910021850-r"]
                  [clucie "0.4.2"]
                  [environ "1.1.0"]]
   :plugins [[lein-codox "0.10.7"]
-            [environ/environ.lein "0.3.1"]]
+            [environ/environ.lein "0.3.1"]
+            [lein-marginalia "0.9.1"]]
+  :java-source-paths ["src/java"]
   :main ^:skip-aot chrondb.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev {:dependencies [[org.clojure/clojure "1.10.1"]]}}
+  :repositories {"jgit-repository" "https://repo.eclipse.org/content/groups/releases/"})
